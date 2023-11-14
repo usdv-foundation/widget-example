5814ddfd0ebc442ac1d8717bf7ebebe9e365bb64
KenYuan1987 <175301075@qq.com>
2023-11-14 18:08:05 +0800

    chore:widget for matrixport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,30 @@ export default function Home() {
     useEffect(() => {
         if (ready) return;
         import('@usdv/usdv-widget').then(({bootstrapWidget, themes}) => {
-            // bootstrapWidget({color: 2, theme:themes.light, isTestnet: false, bridgeRecolorConfig: [
-            //     {
-            //         address: '0x8b532a531aB6962bcA2797425d80fcFf8e440f07',
-            //         chainKey: 'bsc',
-            //     },
-            // ]});
-            bootstrapWidget({color: 3, theme:themes.light, isTestnet: false, bridgeRecolorConfig: [
-                    {
-                        address: '0x292dD933180412923ee47fA73bBF407B6d776B4C',
-                        chainKey: 'avalanche',
-                    },
-                    {
-                        address: '0x5CC0189652Ee881526eD3B8053B21c44e99010B9',
-                        chainKey: 'arbitrum',
-                    },
-                    {
-                        address: '0x5B1d0467BED2e8Bd67c16cE8bCB22a195ae76870',
-                        chainKey: 'bsc',
-                    },
-                    {
-                        address: '0x31691Fd2Cf50c777943b213836C342327f0DAB9b',
-                        chainKey: 'optimism',
-                    },
-                ]});
+            bootstrapWidget({color: 2, theme:themes.light, isTestnet: false, bridgeRecolorConfig: [
+                {
+                    address: '0x8b532a531aB6962bcA2797425d80fcFf8e440f07',
+                    chainKey: 'bsc',
+                },
+            ]});
+            // bootstrapWidget({color: 3, theme:themes.light, isTestnet: false, bridgeRecolorConfig: [
+            //         {
+            //             address: '0x292dD933180412923ee47fA73bBF407B6d776B4C',
+            //             chainKey: 'avalanche',
+            //         },
+            //         {
+            //             address: '0x5CC0189652Ee881526eD3B8053B21c44e99010B9',
+            //             chainKey: 'arbitrum',
+            //         },
+            //         {
+            //             address: '0x5B1d0467BED2e8Bd67c16cE8bCB22a195ae76870',
+            //             chainKey: 'bsc',
+            //         },
+            //         {
+            //             address: '0x31691Fd2Cf50c777943b213836C342327f0DAB9b',
+            //             chainKey: 'optimism',
+            //         },
+            //     ]});
             setReady(true)
         });
         }, [])
