de1ccc06ab5233af60d44ce689e3d8d177cd1aa2
KenYuan1987 <175301075@qq.com>
2023-12-13 22:55:59 +0800

    chore:update widget version and support safe wallet

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-Requested-With, content-type, Authorization',
+                    },
+                ],
+            },
+        ];
+    },
     webpack(config) {
         config.optimization.runtimeChunk = 'single';
         return config;
